8f93b4a73898901a76aab8e8df2c65f5b0a50118
shalu169 <shaliniagrawal16@gmail.com>
2020-02-26 01:36:54 +0530

    knn file added

--- extra-python.py
+++ extra-python.py
@@ -1,6 +1,11 @@
-lst = ['2', '4', '6', '8', '67']
+lst = ['2', '4', '6']
 
 final_data = map(int, lst)
+
+a = b = c, t, u = final_data
+print(b, c, sep='\n')
+
+
 
 #ls = list(final_data)
 for i in final_data:
